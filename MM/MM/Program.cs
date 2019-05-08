272f9999dfdf85758f2f9b7e9103fc219a29bb0f
rosseniv97 <rosseniv97@gmail.com>
2019-05-09 01:33:30 +0300

    MM Console application

---
+++ MM/MM/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int k = 10 * n;
+            char[,] arr= new char[n+1,k];
+                for(int i = 0; i < n+1; i++)
+               {
+
+                for (int j = 0; j < n*10; j++)
+                   {
+
+                    arr[i,j] = '-';
+                   }
+               }
+            for (int i = 0; i < n + 1; i++)
+            {
+               
+                ////////////   FIRST M    /////////////////
+               
+                int j = 0;
+
+                for (j = 0; j < n *2; j++)
+                {
+                    for(int t = 0; t < n; t++)
+                    {
+                        if (j + i == n+t)
+                        {
+                            arr[i, j] = '*';
+                        }
+                       
+                    }
+                }
+                for (j = n; j < n * 3; j++)
+                {
+                    for (int t = 0; t < n; t++)
+                    {
+                        if (j - i == n+t)
+                        {
+                            arr[i, j] = '*';
+                        }
+                    }
+                }
+                for (j = n*2; j < n * 4; j++)
+                {
+                    for (int t = 0; t < n; t++)
+                    {
+                        if (j + i == 3 * n + t)
+                        {
+                            arr[i, j] = '*';
+                        }
+                    }
+                    
+                }
+                for (j = n * 3; j < n * 5; j++)
+                {
+                    for (int t = 0; t < n; t++)
+                    {
+                        if (j - i == 3 * n + t)
+                        {
+                            arr[i, j] = '*';
+                        }
+                    }
+
+                }
+
+                ////////////   SECOND M    /////////////////
+
+                for (j = n*5; j < n * 7; j++)
+                {
+                    for (int t = 0; t < n; t++)
+                    {
+                        if (j + i == 6*n + t)
+                        {
+                            arr[i, j] = '*';
+                        }
+
+                    }
+                }
+
+                for (j = n * 6; j < n * 8; j++)
+                {
+                    for (int t = 0; t < n; t++)
+                    {
+                        if (j - i == 6 * n + t)
+                        {
+                            arr[i, j] = '*';
+                        }
+
+                    }
+                }
+
+                for (j = n * 7; j < n * 9; j++)
+                {
+                    for (int t = 0; t < n; t++)
+                    {
+                        if (j + i == 8 * n + t)
+                        {
+                            arr[i, j] = '*';
+                        }
+
+                    }
+                }
+
+                for (j = n * 8; j < n * 10; j++)
+                {
+                    for (int t = 0; t < n; t++)
+                    {
+                        if (j - i == 8 * n + t)
+                        {
+                            arr[i, j] = '*';
+                        }
+
+                    }
+                }
+
+
+
+
+                }
+
+                for (int i = 0; i < n + 1; i++)
+            {
+                Console.Write('\n');
+
+                for (int j = 0; j < n * 10; j++)
+                {
+
+                        Console.Write(arr[i,j]);
+                    
+                }
+            }
+
+
+            Console.Read();
+        }
+    }
+}
